779c348df381e4d4a21fa024adbc68f801f223ee
Alessandro Faletti <alessandrofaletti@gmail.com>
2018-12-13 00:32:06 -0200

    update

--- Application/src/main/java/com/example/android/bluetoothlegatt/ClientSecurityClass.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/ClientSecurityClass.java
@@ -14,22 +14,6 @@ public class ClientSecurityClass {
 
     public static final String TAG = "SecurityClass_LOG";
 
-    public static String Decrypt(byte[] text) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
-
-        Log.d(TAG, "Bytes = " + text.toString());
-
-        byte [] key = "AAAAA".getBytes("ASCII");
-
-        Cipher rc4 = Cipher.getInstance("RC4");
-        SecretKeySpec rc4Key = new SecretKeySpec(key, "RC4");
-        rc4.init(Cipher.DECRYPT_MODE, rc4Key);
-
-        byte [] cipherText = rc4.update(text);
-
-        return(new String(cipherText, "ASCII"));
-
-    }
-
     public static byte[] Decrypt(byte[] text, SecretKeySpec Ksession) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
 
         Log.d(TAG, "Bytes = " + text.toString());
@@ -38,24 +22,6 @@ public class ClientSecurityClass {
         rc4.init(Cipher.DECRYPT_MODE, Ksession);
 
         byte [] cipherText = rc4.update(text);
-
-        return(cipherText);
-
-    }
-
-    public static byte[] Encrypt(String text) throws UnsupportedEncodingException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
-
-        byte[] plainText = text.getBytes("ASCII");
-
-        Log.i(TAG, text);
-
-        byte [] key = "AAAAA".getBytes("ASCII");
-
-        Cipher rc4 = Cipher.getInstance("RC4");
-        SecretKeySpec rc4Key = new SecretKeySpec(key, "RC4");
-        rc4.init(Cipher.ENCRYPT_MODE, rc4Key);
-
-        byte [] cipherText = rc4.update(plainText);
 
         return(cipherText);
 
